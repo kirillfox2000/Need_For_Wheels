5791ac43658ad9e1a66938bcbb351c51838cfa23
kirillfox2000 <90279866+kirillfox2000@users.noreply.github.com>
2021-12-09 18:14:04 +0300

    Add files via upload

---
+++ Need_For_Wheels/app/src/main/java/ru/bersenev_kirill/need_for_wheels/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.bersenev_kirill.need_for_wheels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
